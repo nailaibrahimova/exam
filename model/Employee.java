5f01a8013fb113a9a628d05c20ef72c008c835d1
nibrahimova <nailaibrahimova62@gmail.com>
2019-08-28 11:06:43 +0400

    changes in model 1

--- model/Employee.java
+++ model/Employee.java
@@ -1,11 +1,104 @@
 package exam.model;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Employee {
+    private int id;
     private String name;
+    private LocalDate birthDate;
     private Gender gender;
-    private LocalDateTime enterDate;
+    private String telephone;
+    private Job job;
     private Employee boss;
+    private double salary;
 
+    private static int nextId=1;
+
+    {
+        id=nextId;
+        nextId++;
+    }
+
+    public Employee(String name, Gender gender, String telephone){
+        this.name=name;
+        this.gender=gender;
+        this.telephone=telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public Employee getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Employee boss) {
+        this.boss = boss;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", gender=" + gender +
+                ", telephone='" + telephone + '\'' +
+                ", job=" + job +
+                ", boss=" + boss +
+                ", salary=" + salary +
+                '}';
+    }
 }
---
+++ model/Job.java
@@ -0,0 +1,45 @@
+package exam.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Job {
+    private String position;
+    private Department department;
+    private LocalDate jobEnterDate;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public LocalDate getJobEnterDate() {
+        return jobEnterDate;
+    }
+
+    public void setJobEnterDate(LocalDate jobEnterDate) {
+        this.jobEnterDate = jobEnterDate;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String date=jobEnterDate.format(dateTimeFormatter);
+        return "Job{" +
+                "position='" + position + '\'' +
+                ", department=" + department +
+                ", jobEnterDate=" + date +
+                '}';
+    }
+}
